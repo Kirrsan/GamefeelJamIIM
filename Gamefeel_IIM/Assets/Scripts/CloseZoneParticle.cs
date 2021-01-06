11192fa20b74acf4410c56af68154af20d87d3e6
Alexis <alexis.sainsard@gmail.com>
2021-01-06 09:35:15 +0100

    gestion spawn particle quand tir proche

---
+++ Gamefeel_IIM/Assets/Scripts/CloseZoneParticle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseZoneParticle : MonoBehaviour
+{
+    public GameObject particlesPrefab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Bullet"))
+        {
+            Vector2 position = this.gameObject.transform.position;
+            GameObject particle = Instantiate(particlesPrefab);
+            particle.transform.position = position;
+            Destroy(particle, 2f);
+        }
+    }
+}
